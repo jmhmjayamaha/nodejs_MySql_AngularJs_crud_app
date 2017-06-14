eda20b5260187dd6ba0b2c4a83144e01b60c6a45
jmhmjayamaha <jmhmjayamaha@gmail.com>
2017-06-14 07:28:18 +0530

    angularjs route added

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+	  $routeProvider
+	  .when("/", {
+		templateUrl : "index.html"
+	})
+	.when("/save", {
+		templateUrl : "pages/save.html"
+	})
+	.when("/update", {
+		templateUrl : "pages/update.html"
+	})
+	.when("/search", {
+		templateUrl : "pages/search.html"
+	});
+});
