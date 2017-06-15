a33dde48aa70843cb92708e6a03b7e1fe8acb875
jmhmjayamaha <jmhmjayamaha@gmail.com>
2017-06-15 13:03:22 +0530

    controller for delete , put

--- js/controller.js
+++ js/controller.js
@@ -20,10 +20,40 @@ app.controller("myController", function($scope, $http) {
     $http.post("http://localhost:8081/api/addUser" , dataObject).then(
       function() {
         alert("successfully added");
+        $scope.getAllUsers();
         $scope.reset();
       }
     );
   }
+
+  $scope.updateUser = function() {
+    var dataObject = {
+      "id": $scope.userId,
+      "name" : $scope.name,
+	    "age" : $scope.age,
+	    "address" : $scope.address
+    };
+
+    $http.put("http://localhost:8081/api/updateUser" , dataObject).then(
+      function() {
+        alert("successfully updated");
+        $scope.getAllUsers();
+        $scope.reset();
+      }
+    );
+  }
+
+  $scope.deleteUserById = function(userId) {
+		$http.delete("http://localhost:8081/api/deleteUser" , {params:{"id": userId}}).then(
+			function() {
+				alert("successfully deleted");
+				$scope.getAllUsers();
+			} ,
+			function() {
+				alert("No such User")
+			}
+		);
+	}
 
   $scope.reset = function() {
     $scope.userId = "";
