45ae0856721fca35f1d7c57ca21442e2af12c0fb
Harshana Madusanka Jayamaha <jmhmjayamaha@gmail.com>
2017-10-22 18:55:37 +0530

    modify the contorller

--- js/app.js
+++ js/app.js
@@ -6,12 +6,15 @@ app.config(function($routeProvider) {
 	// 	templateUrl : "index.html"
 	// })
 	.when("/save", {
-		templateUrl : "pages/save.html"
+		templateUrl : "pages/save.html",
+		controller: "addController"
 	})
 	.when("/update", {
-		templateUrl : "pages/update.html"
+		templateUrl : "pages/update.html",
+		controller : "updateUser"
 	})
 	.when("/search", {
-		templateUrl : "pages/search.html"
+		templateUrl : "pages/search.html",
+		controller : "deleteUser"
 	});
 });
